c4d2b2ffdf2bb1887ca457e05c292b37ef52e093
Danny Parsons <danny@idems.international>
2023-02-23 14:49:23 +0000

    added get_daily_data function

---
+++ R/get_daily_data.R
@@ -0,0 +1,33 @@
+#' Get Daily Data
+#'
+#' @param country 
+#' @param station_id 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_daily_data <- function(country = c("mw", "zm"), station_id) {
+  if (length(country) > 1) stop("'country' must be of length 1")
+  country <- match.arg(country)
+  station_id <- as.character(station_id)
+  dfs <- vector("list", length(station_id))
+  names(dfs) <- station_id
+  print(getwd())
+  for (i in seq_along(station_id)) {
+    f <- paste0(country, "/", "data", "/", station_id[i], ".rds")
+    print(f)
+    if (file.exists(f)) {
+      dfs[[i]] <- readRDS(f)
+    } else {
+      # Un-comment once update_daily_data() written
+      # f <- update_daily_data(country, station_id[i])
+      # dfs[[i]] <- readRDS(f)
+      stop("data not found locally")
+    }
+  }
+  if (length(station_id) > 1) {
+    station_data <- dplyr::bind_rows(dfs)
+  } else station_data <- dfs[[1]]
+  return(station_data)
+}
