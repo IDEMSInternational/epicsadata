79a04ff0d3fbcd07dc14ef5b5af18e86f8bb998b
lilyclements <lily@idems.international>
2023-06-28 18:35:10 +0100

    Get binary file function

--- R/update_metadata.R
+++ R/update_metadata.R
@@ -9,6 +9,5 @@
 update_metadata <- function(country = c("mz", "zm")) {
   filename <- paste0("metadata", ".rds")
   saveto <- paste0(country, "/", filename)
-  invisible(get_data(country = country, 
-                     filename = filename))
+  invisible(get_data(country = country,  filename = filename))
 }
