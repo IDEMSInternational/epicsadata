1acb14ab7099773f010756de03a3e61de626cc97
lilyclements <lily@idems.international>
2024-03-11 14:40:35 +0000

    add summaries to the bucket - function

---
+++ R/add_summary_to_bucket.R
@@ -0,0 +1,43 @@
+#' Add New Summaries to Google Cloud Storage Bucket
+#'
+#' This function adds a new summary data file (RDS format) with a timestamp to a specified Google Cloud Storage (GCS) bucket.
+#'
+#' @param country A character vector specifying the country that the data corresponds to. Options are `"mz"` and `"zm"`.
+#' @param station_id A character string specifying the ID of the station that the data corresponds to.
+#' @param data The data to upload.
+#' @param summary The summary function used to create the data.
+#'
+#' @details
+#' The function creates a timestamp in the format "YYYYMMDDHHMMSS" and appends it to the station_id to form the filename. It then reads the provided file, creates a new file with the timestamped filename, and uploads it to the specified GCS bucket.
+#'
+#' @export
+#' @importFrom googleCloudStorageR gcs_upload
+#' @importFrom jsonlite read_json
+#'
+#' @seealso
+#' \code{get_bucket_name} for retrieving the GCS bucket name.
+#'
+#'
+add_summaries_to_bucket <- function(country, station_id, data, summary) {
+  # Set the GCS bucket name
+  bucket <- get_bucket_name(country)
+  data_dir <- "summaries"
+  
+  # Generate a timestamp
+  timestamp <- format(Sys.time(), format = "%Y%m%d%H%M%S")
+  
+  # Define the filename with the station ID, timestamp, and RDS extension
+  new_filename <- paste0(summary, "_", station_id, ".", timestamp, ".rds")
+  
+  # Custom function to write R object to RDS
+  object_function <- function(input, output) {
+    saveRDS(input, file = output)
+  }
+  
+  # Upload the RDS file to the GCS bucket
+  googleCloudStorageR::gcs_upload(file = data,
+                                  bucket = bucket,
+                                  name = paste0(file.path(data_dir, new_filename)),
+                                  object_function = object_function,
+                                  predefinedAcl = "bucketLevel")
+}
