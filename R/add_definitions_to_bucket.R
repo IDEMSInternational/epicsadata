63fd943c699cc84a49fb28be3ab5dd19b4be3df2
lilyclements <lily@idems.international>
2024-03-27 21:37:48 +0000

    adding functionality with saving definitions data and json

--- R/add_definitions_to_bucket.R
+++ R/add_definitions_to_bucket.R
@@ -5,6 +5,7 @@
 #' @param country A character vector specifying the country or countries from which to get the definitions data. Options are `"mz"` and `"zm"`.
 #' @param station_id A character string specifying the ID of the station for which to get the definitions data.
 #' @param new_definitions A character vector specifying the path to the JSON file containing the new definitions.
+#' @param timestamp A character vector with a timestamp. By default this is `NULL` so is generated.
 #'
 #' @details
 #' The function creates a timestamp in the format "YYYYMMDDHHMMSS" and appends it to the station_id to form the filename. It then reads the provided JSON file, creates a new JSON file with the timestamped filename, and uploads it to the specified GCS bucket.
@@ -16,13 +17,15 @@
 #' @seealso
 #' \code{get_bucket_name} for retrieving the GCS bucket name.
 #'
-add_definitions_to_bucket <- function(country, station_id, new_definitions) {
+add_definitions_to_bucket <- function(country, station_id, new_definitions, timestamp = NULL) {
   # Set the GCS bucket name and the path for the definitions files
   bucket <- get_bucket_name(country)
   definitions_dir <- "definitions"
   
-  # Generate a timestamp
-  timestamp <- format(Sys.time(), format = "%Y%m%d%H%M%S")
+  if (is.null(timestamp)){
+    # Generate a timestamp
+    timestamp <- format(Sys.time(), format = "%Y%m%d%H%M%S") 
+  }
   
   # Define the filename with the timestamp
   new_filename <- paste0(station_id, ".", timestamp, ".json")
