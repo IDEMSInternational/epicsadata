2a1870a80d3feb6bd858bb09837a91a226e2d663
lilyclements <lily@idems.international>
2024-06-26 14:07:35 +0100

    updating bucket names

--- R/get_bucket_name.R
+++ R/get_bucket_name.R
@@ -5,7 +5,7 @@
 #' @return Returns the name of the bucket for the Malawi or Zambia data.
 #'
 #' @examples #get_bucket_name("mw")
-get_bucket_name <- function(country = c("mw", "zm", "zm_test", "ml_test", "ke_test")) {
+get_bucket_name <- function(country = c("mw", "zm", "zm_test", "ml_test", "ke_test", "internal_tests", "zm_workshops", "mw_workshops")) {
   if (length(country) > 1) stop("'country' must be length 1.")
   country <- match.arg(country)
   if (country == "mw") return("malawi_data")
@@ -13,4 +13,7 @@ get_bucket_name <- function(country = c("mw", "zm", "zm_test", "ml_test", "ke_te
   else if (country == "zm_test") return("zambia_test_data")
   else if (country == "ml_test") return("malawi_test_data")
   else if (country == "ke_test") return("kenya_test_data")
+  else if (country == "internal_tests") return("climsoft_data")
+  else if (country == "zm_workshops") return("zm_workshops")
+  else if (country == "mw_workshops") return("mw_workshops")
 }
